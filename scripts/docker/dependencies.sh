b4d64a800900f96f5f2d6487f0c867dc376edbc0
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-04-07 08:52:37 -0400

    Update dependencies.sh

--- scripts/docker/dependencies.sh
+++ scripts/docker/dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Get Docker quick install script.
+curl -fsSL https://get.docker.com/ -o install-docker.sh
+
+# Install docker from script.
+bash install-docker.sh
+
+# Pull the latest images from Docker hub.
 docker pull ubuntu
 docker pull alpine
 docker pull debian
