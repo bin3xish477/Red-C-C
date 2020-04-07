886ba96b0a3cf1eca3603d415ddc6b0e4bfbdb2e
Chris <ckortbaoui@gmail.com>
2020-04-06 22:56:10 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+VERSION=$(lsb_release -a)
+UBUNTU='Ubuntu'
+KALI='Kali'
+
+if [[ "$VERSION" == *"$UBUNTU"* ]];
+then
+    sudo apt-get install python3.6
+fi
+
+if [[ "$VERSION" == *"$KALI"* ]];
+then
+    sudo apt install python3.6
+fi
