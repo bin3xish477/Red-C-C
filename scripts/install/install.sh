667e44d68d1f03f6333c1a005e0c1824658f9bf0
Chris <ckortbaoui@gmail.com>
2020-04-07 15:44:00 -0400

    Update install.sh

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -4,12 +4,16 @@ VERSION=$(lsb_release -a)
 UBUNTU='Ubuntu'
 KALI='Kali'
 
+cd /tmp/
+
 if [[ "$VERSION" == *"$UBUNTU"* ]];
 then
     sudo apt-get install python3.6
+	sudo apt-get install wget
 fi
 
 if [[ "$VERSION" == *"$KALI"* ]];
 then
     sudo apt install python3.6
+	sudo apt install wget
 fi
